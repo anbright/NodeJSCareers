ed43d4163d6586556f3b68ad6bdb1c3175b6cfee
anbright <anbright@sas.upenn.edu>
2017-04-26 15:50:58 -0400

    Fixed /details companies

--- CIS450ProjectNode/routes/details.js
+++ CIS450ProjectNode/routes/details.js
@@ -1,12 +1,38 @@
 var express = require('express');
 var router = express.Router();
 var User = require('../models/user')
+var oracledb = require('oracledb')
 
 
 /* GET profile page. */
 router.get('/', function(req, res, next) {
     if ((req.session && req.session.user)) {
-        res.render('details');
+      oracledb.getConnection({
+              // The connection details to my database
+              user: "cis450project",
+              password: "tahmidisabitch",
+              connectString: "cis450project.creb8qtnnbvb.us-west-2.rds.amazonaws.com:1521/DBPROJ"
+          },
+          function(err, connection) {
+              // If an error happens during establishing the connection, print the error message and return (exit the program)
+              if (err) {
+                  console.error(err.message);
+                  return;
+              }
+              // Executing my SQL SELECT statement against the departments table
+              connection.execute(
+                  "SELECT NAME FROM COMPANY ORDER BY NAME ASC",
+                  function(err, result) {
+                      // If an error happens during the SQL execution, print the error message and return (exit the program)
+                      if (err) {
+                          console.error(err.message);
+                          return;
+                      }
+                      console.log(result.rows)
+                      res.render('details', {companies: result.rows});
+                  });
+          });
+
     } else {
         res.redirect('/login');
     }
